8b561003f7693ae7871d377c655f99d9aebd3dd2
Shuvranil <shuvranilmondal@gmail.com>
2021-04-17 10:48:19 +0530

    add auto joke function

--- js-file/file.js
+++ js-file/file.js
@@ -8,3 +8,39 @@ txtColor.addEventListener('mouseup',chgColor)
 txtColor.addEventListener('mousedown',()=>{
     txtColor.style.background='#CCCC00'
 })
+
+
+let result = document.querySelector('#result')
+let play = document.querySelector("#play")
+
+let newjokes =()=>{
+
+    let setheader = {
+        headers : {
+            Accept : "application/json"
+        }
+    }
+
+
+    fetch('https://icanhazdadjoke.com/',setheader)
+    .then((res)=>res.json())
+    .then((data)=>{
+        result.innerHTML = data.joke
+    }).catch((error)=>{
+        console.log(error)
+    })
+    
+
+
+}
+
+
+
+
+play.addEventListener('click',function(){
+    result.innerHTML = 'Loading your joke...'
+    setTimeout(() => {
+        newjokes()
+    }, 1000);
+})
+newjokes()
